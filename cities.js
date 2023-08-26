47ff3618434fd58ea1b62c339fc1d0a55a11efa7
Weather-App_JavaScript_HTML_CSS <surajrenake3@gmail.com>
2023-08-26 13:25:17 +0530

    files added

---
+++ cities.js
@@ -0,0 +1,315 @@
+export default [
+  {
+    country: "PK",
+    name: "Abbottabad",
+    lat: "34.1463",
+    lng: "73.21168",
+  },
+  {
+    country: "PK",
+    name: "Adilpur",
+    lat: "27.93677",
+    lng: "69.31941",
+  },
+  {
+    country: "PK",
+    name: "Ahmadpur East",
+    lat: "29.14269",
+    lng: "71.25771",
+  },
+  {
+    country: "IN",
+    name: "Bangalore",
+    lat: "12.972442",
+    lng: "77.580643",
+  },
+  {
+    country: "PK",
+    name: "Akora",
+    lat: "34.00337",
+    lng: "72.12561",
+  },
+  {
+    country: "PK",
+    name: "Aliabad",
+    lat: "36.30703",
+    lng: "74.61545",
+  },
+  {
+    country: "PK",
+    name: "Alik Ghund",
+    lat: "30.48976",
+    lng: "67.52177",
+  },
+  
+  {
+    country: "IN",
+    name: "Mysuru",
+    lat: "12.365833",
+    lng: "76.613889"
+  },
+  {
+    country: "IN",
+    name: "Hyderabad",
+    lat: "17.375278",
+    lng: "78.474444"
+  },
+  {
+    country: "IN",
+    name: "Pune",
+    lat: "18.5203",
+    lng: "73.8567"
+  },
+  {
+    country: "IN",
+    name: "Ahmedabad",
+    lat: "23.03",
+    lng: "72.58"
+  },
+  {
+    country: "IN",
+    name: "Chennai",
+    lat: "13.0825",
+    lng: "80.275"
+  },
+  {
+    country: "IN",
+    name: "Kolkata",
+    lat: "22.5675",
+    lng: "88.37"
+  },
+  {
+    country: "IN",
+    name: "Delhi",
+    lat: "28.61",
+    lng: "77.23"
+  },
+  {
+    country: "IN",
+    name: "Mumbai",
+    lat: "19.0761",
+    lng: "72.8775"
+  },
+  {
+    country: "IN",
+    name: "Mysore",
+    lat: "12.313214",
+    lng: "76.633332"
+  },
+  {
+    country: "IN",
+    name: "Hubli",
+    lat: "15.3303692",
+    lng: "75.1208977"
+  },
+  {
+    country: "IN",
+    name: "Dharwad",
+    lat: "15.458925",
+    lng: "75.007808"
+  },
+  {
+    country: "IN",
+    name: "Gadag",
+    lat: "15.431795",
+    lng: "75.626165"
+  },
+  {
+    country: "IN",
+    name: "Mangalore",
+    lat: "12.816667",
+    lng: "74.833333"
+  },
+  {
+    country: "IN",
+    name: "Bellary",
+    lat: "14.616667",
+    lng: "76.966667"
+  },
+  {
+    country: "IN",
+    name: "Gulbarga",
+    lat: "17.416667",
+    lng: "76.616667"
+  },
+  {
+    country: "IN",
+    name: "Bijapur",
+    lat: "15.916667",
+    lng: "75.75"
+  },
+  {
+    country: "IN",
+    name: "Raichur",
+    lat: "15.25",
+    lng: "77.333333"
+  },
+  {
+    country: "IN",
+    name: "Udupi",
+    lat: "13.75",
+    lng: "74.833333"
+  },
+  {
+    country: "IN",
+    name: "Bengaluru",
+    lat: "12.9634",
+    lng: "77.6193"
+  },
+  {
+    country: "IN",
+    name: "Mangalore",
+    lat: "12.625",
+    lng: "777.5855"
+  },
+  {
+    country: "IN",
+    name: "Gulbarga",
+    lat: "17.383333",
+    lng: "76.616667"
+  },
+  {
+    country: "IN",
+    name: "Davangere",
+    lat: "15.116667",
+    lng: "75.75"
+  },
+  {
+    country: "IN",
+    name: "Mysore",
+    lat: "12.316667",
+    lng: "76.6"
+  },
+  {
+    country: "IN",
+    name: "Gokak",
+    lat: "16.75",
+    lng: "75.666667"
+  },
+  {
+    country: "IN",
+    name: "Aland",
+    lat: "15.666667",
+    lng: "75.2"
+  },
+  {
+    country: "IN",
+    name: "Bhalki",
+    lat: "17.3",
+    lng: "76.583333"
+  },
+  {
+    country: "IN",
+    name: "Khanapur",
+    lat: "15.916667",
+    lng: "75.333333"
+  },
+  {
+    country: "IN",
+    name: "Ramdurg",
+    lat: "17.25",
+    lng: "76.366667"
+  },
+  {
+    country: "IN",
+    name: "Savanur",
+    lat: "16.416667",
+    lng: "75.583333"
+  },
+  {
+    country: "IN",
+    name: "Jamkhandi",
+    lat: "17.083333",
+    lng: "76.333333"
+  },
+  {
+    country: "IN",
+    name: "Anegundi",
+    lat: "15.116667",
+    lng: "76.166667"
+  },
+  {
+    country: "IN",
+    name: "Mudhol",
+    lat: "16.8",
+    lng: "75.833333"
+  },
+  {
+    country: "IN",
+    name: "Chikodi",
+    lat: "15.416667",
+    lng: "75.666667"
+  },
+  {
+    country: "IN",
+    name: "Chincholi",
+    lat: "16.166667",
+    lng: "75.25"
+  },
+  {
+    country: "IN",
+    name: "Belagavi",
+    lat: "15.849695",
+    lng: "74.497674"
+  },
+  {
+      country: "IN",
+      name: "New Delhi",
+      lat: "28.613942",
+      lng: "77.208825"
+    },
+    {
+      country: "US",
+      name: "New York City",
+      lat: "40.712784",
+      lng: "-74.005941"
+    },
+    {
+      country: "CN",
+      name: "Beijing",
+      lat: "39.907501",
+      lng: "116.407722"
+    },
+    {
+      country: "JP",
+      name: "Tokyo",
+      lat: "35.689500",
+      lng: "139.691711"
+    },
+    {
+      country: "BR",
+      name: "São Paulo",
+      lat: "-23.550556",
+      lng: "-46.636111"
+    },
+    {
+      country: "UK",
+      name: "London",
+      lat: 51.500000,
+      lng: -0.125000
+    },
+    {
+      country: "FR",
+      name: "Paris",
+      lat: 48.856614,
+      lng: 2.350278
+    },
+    {
+      country: "DE",
+      name: "Berlin",
+      lat: 52.516667,
+      lng: 13.383333
+    },
+    {
+      country: "RU",
+      name: "Moscow",
+      lat: 55.755831,
+      lng: 37.617776
+    },
+    {
+      country:"IN",
+      name:"Diu",
+      lat:"20.7",
+      lng:"70.9833"
+    }
+];
